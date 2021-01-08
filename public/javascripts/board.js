e1efd824d2177a26c17ecd29bb7675ef0675bcfe
david <david@pc>
2021-01-08 22:00:56 +0100

    Add game grid

---
+++ public/javascripts/board.js
@@ -0,0 +1,23 @@
+(function () {
+    let cellBlack = '<div class="black">g</div>';
+    let cellWhite = '<div class="white">g</div>';
+    
+    let boardContainer = document.getElementById('board_container');
+    boardContainer.className = "container";
+    for(var i = 0; i < 8; i++) {
+	for(var j = 0; j < 8; j++) {
+	    let cell = document.createElement("div");
+	    cell.id = (i+1).toString()+"-"+(j+1).toString();
+	    cell.innerHTML = cell.id;
+	    if((i+j) % 2 == 0) {
+		cell.className = "black";
+		} else {
+		    cell.className = "white";
+		}
+	    boardContainer.insertAdjacentElement("beforeend", cell);
+	    cell.addEventListener("click", function cellClick(e) {
+		console.log(e);
+	    });
+	}
+    }
+})();
