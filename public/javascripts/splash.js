e0c7609e1b6fedb6786b79733693f770f9d12c3e
mart-kuil <64152035+mart-kuil@users.noreply.github.com>
2021-01-12 14:11:36 +0100

    Update splash.js

--- public/javascripts/splash.js
+++ public/javascripts/splash.js
@@ -1,44 +1,28 @@
 	
-(funciont(){
+function(){
+
+
 	const socket = new WebSocket("ws://localhost:3000");
-	socket.onmessage =  function(event){
+	socket.onopen =  function(event){
+
+		let msg = messages.STATISTICS_REQUEST;
+		socket.send(JSON.stringify(msg));
+		console.log("hi this works");
+	}
+
+	
+
+	socket.onmessage = function(){
 		let msg = JSON.parse(event.data);
-		console.log(msg);
 		if(msg.kind===messages.kind.STATISTICS_REQUEST){
 			let stats = JSON.parse(msg.data);
-
-		}
-
-	}
-
-
-
-	function statUpdate(){
-		fastestGame.textContent = fastestTime;
-		totalTimeSpent.textContent = sumAllTimes;
-		totalGamesPlayed.textContent = startedGames;
-	}
-
-	 statAdded(){
-		var sumAllTimes = 0;
-		var playedGames = 0;
-		var fastestGame;
-
-		/from board.js/
-		var GAME_STARTED;
-		var GAME_WON;
-		
-
-		if(GAME_WON){
-			playedGames++;
-			sumAllTimes += gameStopTime;
-			if(gameStopTime<fastestTime){
-				fastestTime = gameStopTime;
-				
-			}
-			
-
+			let totalTime = stats.time;
+			let openRooms = stats.openRooms;
+			let currentGames = stats.gamesNumber;
+			console.log(totalTime);
+			console.log(openRooms);
+			console.log(currentGames)
+			//statUpdate();
 		}
 	}
 }	
-)
