0ba650a59e49e24cd5c694d754172e8bbc73aad4
mart-kuil <64152035+mart-kuil@users.noreply.github.com>
2021-01-14 16:48:48 +0100

    Implemented ability to enter statistics

--- public/javascripts/splash.js
+++ public/javascripts/splash.js
@@ -4,11 +4,12 @@
 
 
 	const socket = new WebSocket("ws://localhost:3000");
-	socket.onopen =  function(event){
-
+	socket.onopen = function(event){
+		htmlString.textContent = "lets see if this works"
+		console.log("hi this works");
 		let msg = messages.STATISTICS_REQUEST;
 		socket.send(JSON.stringify(msg));
-		console.log("hi this works");
+
 	}
 
 	
@@ -16,10 +17,12 @@
 	socket.onmessage = function(event){
 		let msg = JSON.parse(event.data);
 	    if(msg.kind===messages.kind.STATISTICS_REQUEST){
-		console.log(msg)
-			let totalTime = msg.time;
-			let openRooms = msg.openRooms;
-			let currentGames = msg.gamesNumber;
+			console.log(msg)
+			totalTime.textContent = "The total time played is: " + msg.time;
+			openRooms.textContent = "The amount of open rooms at this moment is: "+msg.openRooms;
+			currentGames.textContent = "The amount of games currently played is: "+msg.gamesNumber;
+
 		}
+
 	}
 })();
