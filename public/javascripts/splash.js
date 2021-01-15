a5e3a2a2b7f95f8aa561efe8baec326470cc9521
david <david@pc>
2021-01-15 14:43:56 +0100

    Fixed statistics in splash screen

--- public/javascripts/splash.js
+++ public/javascripts/splash.js
@@ -4,10 +4,8 @@
 
 
 	const socket = new WebSocket("ws://localhost:3000");
-	socket.onopen = function(event){
-		htmlString.textContent = "lets see if this works"
-		console.log("hi this works");
-		let msg = messages.STATISTICS_REQUEST;
+    socket.onopen = function(event){
+	let msg = messages.STATISTICS_REQUEST;
 		socket.send(JSON.stringify(msg));
 
 	}
@@ -17,12 +15,11 @@
 	socket.onmessage = function(event){
 		let msg = JSON.parse(event.data);
 	    if(msg.kind===messages.kind.STATISTICS_REQUEST){
-			console.log(msg)
-			totalTime.textContent = "The total time played is: " + msg.time;
-			openRooms.textContent = "The amount of open rooms at this moment is: "+msg.openRooms;
-			currentGames.textContent = "The amount of games currently played is: "+msg.gamesNumber;
-
-		}
+		console.log(msg)
+		document.getElementById("totalTime").textContent = "The total time played is: " + msg.data.time;
+		document.getElementById("openRooms").textContent = "The amount of open rooms at this moment is: "+msg.data.openRooms;
+		document.getElementById("numPlayers").textContent = "The amount of players is: "+msg.data.playerNumber;
+	    }
 
 	}
 })();
